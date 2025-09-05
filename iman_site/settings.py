3a6b8f2e6eafe45c8d07929060262fbcb019ffd6
Benson-af <owusubensonkwabena@gmail.com>
2025-09-05 11:52:32 +0000

    Update settings and configuration

--- iman_site/settings.py
+++ iman_site/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['iman-qjgv.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['iman-qjgv.onrender.com', 'iman-jibril2.onrender.com'
+                ,'localhost', '127.0.0.1']
 
 # Application definition
 
